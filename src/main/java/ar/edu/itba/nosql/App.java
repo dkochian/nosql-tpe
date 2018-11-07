65eb0e5e6942d4f25273f71a7de9e43191b7324e
Juan P. Dantur <jpdantur@gmail.com>
2018-11-07 16:22:19 -0300

    main

--- src/main/java/ar/edu/itba/nosql/App.java
+++ src/main/java/ar/edu/itba/nosql/App.java
@@ -1,5 +1,13 @@
 package ar.edu.itba.nosql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String password = "root";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * from trajectories where userid=0 order by tpos")) {
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1)+ "|" + rs.getString(2)+"|"+rs.getString(3));
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(App.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
     }
 }
