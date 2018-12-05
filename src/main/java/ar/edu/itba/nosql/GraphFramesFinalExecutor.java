18ed6382ee48811fdceb1c93c5b8aba03de99b01
mperazzo <maperazzo@itba.edu.ar>
2018-12-05 12:57:41 -0300

    fixes

--- src/main/java/ar/edu/itba/nosql/GraphFramesFinalExecutor.java
+++ src/main/java/ar/edu/itba/nosql/GraphFramesFinalExecutor.java
@@ -10,47 +10,65 @@ import org.graphframes.GraphFrame;
 
 import static org.apache.spark.sql.functions.collect_list;
 import static org.apache.spark.sql.functions.count;
+import static org.apache.spark.sql.functions.max;
 
 public class GraphFramesFinalExecutor {
 
     private static final String FILE_NAME_INPUT = "mapping";
+    private static final Integer QUERY_NUMBER = 1;
 
     public static void main(String[] args) {
 
         SparkSession sp = SparkSession.builder().appName("FinalQueryExecutor").getOrCreate();
-        JavaSparkContext sparkContext= new JavaSparkContext(sp.sparkContext());
+        JavaSparkContext sparkContext = new JavaSparkContext(sp.sparkContext());
         SQLContext sqlContext = new SQLContext(sp);
 
         Pair<Dataset<Row>, Dataset<Row>> nodesAndEdges = LoadNodesAndEdges(sqlContext);
 
         GraphFrame graph = GraphFrame.apply(nodesAndEdges.getKey(), nodesAndEdges.getValue());
 
-        //Query1(graph).show(Integer.MAX_VALUE);
+        final long start = System.currentTimeMillis();
+
+        if (QUERY_NUMBER == 1)
+            Query1(graph).show(Integer.MAX_VALUE);
+        else if (QUERY_NUMBER == 2)
+            Query2(graph).show(Integer.MAX_VALUE);
+
+        System.out.println("System.currentTimeMillis() - start = " + (System.currentTimeMillis() - start));
 
         sparkContext.close();
     }
 
+    private static Dataset<Row> Query1(GraphFrame graph) {
+        return graph.find("(s)")
+                .filter("s.label='Stop'")
+                .groupBy("s.userId")
+                .agg(max("s.tpos").alias("length"))
+                .select("userId", "length");
+
+    }
+
+    private static Dataset<Row> Query2(GraphFrame graph) {
+        return graph.find("(s)-[e1]->(v); (v)-[e2]->(cat); (cat)-[e3]->(c)")
+                .filter("e1.label='isVenue' and e2.label='hasCategory' and e3.label='subCategoryOf'")
+                .filter("s.label='Stop' and v.label='Venue' and cat.label='Categories' and c.label='Category'")
+                .filter("c.secondId='Airport'")
+                .groupBy("s.userId")
+                .agg(count("s.id").alias("cantidad"))
+                .select("userId", "cantidad");
+    }
+
     private static Dataset<Row> QueryTesting(GraphFrame graph) {
+        return graph.find("(s)")
+                .filter("s.label='Stop'")
+                .agg(count("s.id").alias("count"))
+                .select("count");
+    }
 
-        final Dataset<Row> query1 = graph.find("(s1)-[e11]->(v1); (v1)-[e12]->(cat1); (cat1)-[e13]->(c1); " +
-                "(s2)-[e21]->(v2); (v2)-[e22]->(cat2); (cat2)-[e23]->(c2); " +
-                "(s3)-[e31]->(v3); (v3)-[e32]->(cat3); (cat3)-[e33]->(c3); " +
-                "(s1)-[e1]->(s2); (s2)-[e2]->(s3)")
-                .filter("e11.label='isVenue' and e21.label='isVenue' and e31.label='isVenue'")
-                .filter("e12.label='hasCategory' and e22.label='hasCategory' and e32.label='hasCategory'")
-                .filter("e13.label='subCategoryOf' and e23.label='subCategoryOf' and e33.label='subCategoryOf'")
-                .filter("e1.label='trajStep' and e2.label='trajStep'")
-                .filter("s1.label='Stop' and s2.label='Stop' and s3.label='Stop'")
-                .filter("v1.label='Venue' and v2.label='Venue' and v3.label='Venue'")
-                .filter("cat1.label='Categories' and cat2.label='Categories' and cat3.label='Categories'")
-                .filter("c1.label='Category' and c2.label='Category' and c3.label='Category'")
-                .filter("c1.secondId='Home' and c2.secondId='Station' and     c3.secondId='Airport'")
-                .distinct()
-                .groupBy("s1.userId")
-                .agg(collect_list("s1.tpos").alias("from"))
-                .select("userId", "from");
-
-        return query1;
+    private static Dataset<Row> QueryTesting2(GraphFrame graph) {
+        return graph.find("(s)")
+                .filter("s.label='Stop'")
+                .select( "s");
     }
 
 
--- src/main/java/ar/edu/itba/nosql/Neo4jSparkConnectorMain.java
+++ src/main/java/ar/edu/itba/nosql/Neo4jSparkConnectorMain.java
@@ -1,92 +1,82 @@
 package ar.edu.itba.nosql;
 
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.*;
-import org.graphframes.GraphFrame;
-import org.neo4j.spark.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.neo4j.spark.Neo4j;
 import scala.Tuple2;
 import scala.collection.Seq;
 
-import javax.xml.crypto.Data;
 import java.util.LinkedList;
 import java.util.List;
-
-import static org.apache.spark.sql.functions.count;
 
 
 public class Neo4jSparkConnectorMain {
 
-	private static final String FILE_NAME_OUTPUT = "mapping";
+    private static final String FILE_NAME_OUTPUT = "mapping";
 
-	public static void main (String[] args){
+    public static void main(String[] args) {
         SparkSession sp = SparkSession.builder().appName("Neo4j Connector")
-                .config("spark.neo4j.bolt.url","bolt://node1.it.itba.edu.ar:9689")
-                .config("spark.neo4j.bolt.user","jdantur")
-                .config("spark.neo4j.bolt.password","jdantur")
+                .config("spark.neo4j.bolt.url", "bolt://node1.it.itba.edu.ar:9689")
+                .config("spark.neo4j.bolt.user", "jdantur")
+                .config("spark.neo4j.bolt.password", "jdantur")
                 .getOrCreate();
         JavaSparkContext jsc = new JavaSparkContext(sp.sparkContext());
 
-		Neo4j neo = new Neo4j(jsc.sc());
+        Neo4j neo = new Neo4j(jsc.sc());
 
 
-		Neo4j stops = neo.cypher("MATCH (s:Stop) RETURN s.id as id, null as secondId, s.userId as userId, s.utctimestamp as utctimestamp, s.tpos as tpos, labels(s)(1) as label",
-				new scala.collection.immutable.HashMap<>());
+        Neo4j stops = neo.cypher("MATCH (s:Stop) RETURN s.id as id, null as secondId, s.userId as userId, s.utctimestamp as utctimestamp, s.tpos as tpos, labels(s)[0] as label;",
+                new scala.collection.immutable.HashMap<>());
 
-		Neo4j venues = neo.cypher("MATCH (v:Venue) RETURN id(v) as id, v.id as secondId, null as userId, null as utctimestamp, null as tpos, labels(v)(1) as label",
-				new scala.collection.immutable.HashMap<>());
+        Dataset<Row> stopsdf = stops.loadDataFrame();
 
-		Neo4j categories = neo.cypher("MATCH (c:Categories) RETURN id(c) as id, c.name as secondId, null as userId, null as utctimestamp, null as tpos, labels(c)(1) as label",
-				new scala.collection.immutable.HashMap<>());
+        Neo4j venues = neo.cypher("MATCH (v:Venue) RETURN id(v) as id, v.id as secondId, null as userId, null as utctimestamp, null as tpos, labels(v)[0] as label",
+                new scala.collection.immutable.HashMap<>());
 
-		Neo4j category = neo.cypher("MATCH (c:Category) RETURN id(c) as id, c.name as secondId, null as userId, null as utctimestamp, null as tpos, labels(c)(1) as label",
-				new scala.collection.immutable.HashMap<>());
+        Dataset<Row> venuesdf = venues.loadDataFrame();
 
-		Dataset<Row> nodesdf = stops.loadDataFrame(GraphFramesSchema()).union(venues.loadDataFrame(GraphFramesSchema()))
-				.union(categories.loadDataFrame(GraphFramesSchema()))
-				.union(category.loadDataFrame(GraphFramesSchema()));
+        Neo4j categories = neo.cypher("MATCH (c:Categories) RETURN id(c) as id, c.name as secondId, null as userId, null as utctimestamp, null as tpos, labels(c)[0] as label",
+                new scala.collection.immutable.HashMap<>());
 
-		Neo4j edges = neo.cypher("MATCH (n)-[x]->(n1) RETURN id(n) as src, type(x) as label, id(n1) as dst ",
-				new scala.collection.immutable.HashMap<>());
+        Dataset<Row> categoriesdf = categories.loadDataFrame();
 
-		Dataset<Row> edgesdf = edges.loadDataFrame();
+        Neo4j category = neo.cypher("MATCH (c:Category) RETURN id(c) as id, c.name as secondId, null as userId, null as utctimestamp, null as tpos, labels(c)[0] as label",
+                new scala.collection.immutable.HashMap<>());
 
-		GraphFrame graph = GraphFrame.apply(nodesdf, edgesdf);
+        Dataset<Row> categorydf = category.loadDataFrame();
 
-		graph.vertices().show(Integer.MAX_VALUE);
-		graph.edges().show(Integer.MAX_VALUE);
-		//Query1(graph).show(Integer.MAX_VALUE);
-		//write(nodesdf, edgesdf);
+        Dataset<Row> nodesdf = stopsdf
+                .union(venuesdf)
+                .union(categoriesdf)
+                .union(categorydf);
+
+        Neo4j edges = neo.cypher("MATCH (n)-[x]->(n1) RETURN id(n) as src, type(x) as label, id(n1) as dst ",
+                new scala.collection.immutable.HashMap<>());
+
+        Dataset<Row> edgesdf = edges.loadDataFrame();
+
+        write(nodesdf, edgesdf);
 
         jsc.close();
 
     }
 
-	private static Seq<Tuple2<String, String>> GraphFramesSchema() {
-		List<Tuple2<String, String>> schema = new LinkedList<>();
-		schema.add(new Tuple2<>("id", "long"));
-		schema.add(new Tuple2<>("secondId", "string"));
-		schema.add(new Tuple2<>("userId", "long"));
-		schema.add(new Tuple2<>("utctimestamp", "string"));
-		schema.add(new Tuple2<>("tpos", "long"));
-		schema.add(new Tuple2<>("label", "string"));
-		return scala.collection.JavaConversions.asScalaBuffer(schema).toSeq();
-	}
+    private static Seq<Tuple2<String, String>> GraphFramesSchema() {
+        List<Tuple2<String, String>> schema = new LinkedList<>();
+        schema.add(new Tuple2<>("id", "long"));
+        schema.add(new Tuple2<>("secondId", "string"));
+        schema.add(new Tuple2<>("userId", "long"));
+        schema.add(new Tuple2<>("utctimestamp", "string"));
+        schema.add(new Tuple2<>("tpos", "long"));
+        schema.add(new Tuple2<>("label", "string"));
+        return scala.collection.JavaConversions.asScalaBuffer(schema).toSeq();
+    }
 
-	private static void write (Dataset<Row> nodes, Dataset<Row> edges) {
-		nodes.write().mode(SaveMode.Overwrite).parquet("hdfs:///user/maperazzo/" + FILE_NAME_OUTPUT + "_nodes");
-		edges.write().mode(SaveMode.Overwrite).parquet("hdfs:///user/maperazzo/" + FILE_NAME_OUTPUT + "_edges");
-	}
-
-	private static Dataset<Row> Query1(GraphFrame graph) {
-
-		final Dataset<Row> query1 = graph.find("(s)-[e1]->(v); (v)-[e2]->(cat); (cat)-[e3]->(c)")
-				.filter("e1.label='isVenue' and e2.label='hasCategory' and e3.label='subCategoryOf'")
-				.filter("s.label='Stop' and v.label='Venue' and cat.label='Categories' and c.label='Category'")
-				.filter("c.secondId='Airport'")
-				.groupBy("s.userId")
-				.agg(count("s.id").alias("cantidad"))
-				.select("userId", "cantidad");
-
-		return query1;
-	}
+    private static void write(Dataset<Row> nodes, Dataset<Row> edges) {
+        nodes.write().mode(SaveMode.Overwrite).parquet("hdfs:///user/maperazzo/" + FILE_NAME_OUTPUT + "_nodes");
+        edges.write().mode(SaveMode.Overwrite).parquet("hdfs:///user/maperazzo/" + FILE_NAME_OUTPUT + "_edges");
+    }
 }
